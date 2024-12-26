a9a52120ad03149ca5024c435f8398b660bb6f4f
Khursandbek <axmadjonovxursandbek7@gmail.com>
2024-12-26 21:53:02 +0900

    MITASK qo'shildi, vazifa bajarildi

--- src/train.ts
+++ src/train.ts
@@ -1,17 +1,18 @@
-// M-TASK:
+// M-TASK
 
-// Shunday function yozing, u raqamlardan tashkil topgan array qabul qilsin va array ichidagi
-// har bir raqam uchun raqamni ozi va hamda osha raqamni kvadratidan tashkil topgan object hosil qilib,
-// hosil bolgan objectlarni array ichida qaytarsin.
-// MASALAN: getSquareNumbers([1, 2, 3]) return [{number: 1, square: 1}, {number: 2, square: 4}, {number: 3, square: 9}];
-
-function getSquareNumbers(arr: number[]) {
-  return arr.map((ele) => {
-    return { number: ele, square: ele * ele };
-  });
+function getSquareNumbers(
+  numbers: number[]
+): { number: number; square: number }[] {
+  return numbers.map((num) => ({
+    number: num,
+    square: num * num,
+  }));
 }
 
-console.log(getSquareNumbers([1, 2, 3])); // [{number: 1, square: 1}, {number: 2, square: 4}, {number: 3, square: 9}]
+// Misol
+const result = getSquareNumbers([1, 2, 3]);
+console.log(result);
+// Natija: [{number: 1, square: 1}, {number: 2, square: 4}, {number: 3, square: 9}]
 
 // L-TASK:
 // Shunday function yozing, u string qabul qilsin va string ichidagi hamma sozlarni chappasiga yozib
