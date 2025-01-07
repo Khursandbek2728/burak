cccf0f657250829ef4ec336b6a3c79a0d18c5985
Khursandbek <axmadjonovxursandbek7@gmail.com>
2025-01-07 19:31:00 +0900

    O TASK completed

--- src/train.ts
+++ src/train.ts
@@ -186,3 +186,5 @@ console.log(calculateSumOfNumbers([10, "10", { son: 10 }, true, 35]));
 console.log(calculateSumOfNumbers([12, "2", { son: 5 }, false, 98]));
 console.log(calculateSumOfNumbers([71, "20", { son: 1 }, false, 18]));
 console.log(calculateSumOfNumbers([0, "101", { son: 9 }, true, 2]));
+
+//sdkjbkjsdkdhvkdhdssdhkshv
