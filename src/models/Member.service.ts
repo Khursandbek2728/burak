5b7e98467fbcc0a268cd4f4e40b703528877a75b
Token <axmadjonovxursandbek7@gmail.com>
2025-01-26 18:37:20 +0900

    fix: modify  getMemberDetail business logic

--- src/models/Member.service.ts
+++ src/models/Member.service.ts
@@ -60,6 +60,17 @@ class MemberService {
     return result as unknown as Member;
   }
 
+  public async getMemberDetail(member: Member): Promise<Member> {
+    const memberId = shapeIntoMongooseObjectId(member._id);
+    const result = await this.memberModel
+      .findOne({ _id: memberId, memberStatus: MemberStatus.ACTIVE })
+      .exec();
+
+    if (!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
+
+    return result as unknown as Member;
+  }
+
   /** SSR */
   public async processSignup(input: MemberInput): Promise<Member> {
     const exist = await this.memberModel
